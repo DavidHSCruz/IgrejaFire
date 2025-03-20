0cf497733e44598a2a62052bc81a52d1d1e52e68
DavidHSCruz <david.hs.cruz@gmail.com>
2025-03-20 00:59:56 -0300

    Envia res formulário para whats

--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Input } from "../Input/Input"
-import axios from "axios"
+// import axios from "axios"
 
 interface FormularioProps {
   type: 'contato' | 'inscricao'
@@ -16,6 +16,7 @@ export const Formulario = ({type, className}: FormularioProps) => {
   })
 
   const [telefone, setTelefone] = useState<string>("")
+  const [nascimento, setNascimento] = useState<string>("")
 
   function reescreveTelefone(e: React.ChangeEvent<HTMLInputElement>) {
     let data = e.target.value.replace(/[^\d]/g, '')
@@ -34,74 +35,116 @@ export const Formulario = ({type, className}: FormularioProps) => {
       handleChange(e)
 }
 
+function reescreveNascimento(e: React.ChangeEvent<HTMLInputElement>) {
+  let data = e.target.value.replace(/[^\d]/g, '')
+
+  if (data.length <= 3) {
+      data = data.replace(/(\d{1,2})/, '$1/ ')
+    } else if (data.length <= 6) {
+      data = data.replace(/(\d{2})(\d{1,2})/, '$1/$2/')
+    } else {
+      data = data.replace(/(\d{2})(\d{2})(\d{1,4})/, '$1/$2/$3')
+    }
+
+    setNascimento(data)
+    handleChange(e)
+}
+
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const telefone = e.target.value.replace(/[ ()-]/g, '')
+    const nascimento = e.target.value.replace(/[/]/g, '-')
+    let modificado
+
+    if (e.target.name === 'telefone') {
+      modificado = telefone
+    } else if (e.target.name === 'data de nascimento') {
+      modificado = nascimento
+    }else {
+      modificado = e.target.value
+    }
+
     setFormData({
       ...formData, 
-      telefone: formData.telefone.replace(/[ ()-]/g, ''), 
-      [e.target.name]: e.target.value 
+      [e.target.name]: modificado
     })
   }
   
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  // async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault()
+
+  //   try {
+  //     const response = await axios.post("http://localhost:5000/membros", formData);
+  //     console.log("Membro criado com sucesso:", response.data)
+  //     alert("Membro cadastrado!")
+  //   } catch (error: unknown) {
+  //     let errorMessage = 'Erro desconhecido'
+  //     if (axios.isAxiosError(error)) {
+  //       errorMessage = error.response?.data || error.message
+  //     }
+  //     console.error("Erro ao cadastrar membro:", error)
+  //     alert("Erro ao cadastrar membro, " + errorMessage)
+  //   }
+  // }
+
+  function handleSubmitWhatsapp(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
 
-    try {
-      const response = await axios.post("http://localhost:5000/membros", formData);
-      console.log("Membro criado com sucesso:", response.data)
-      alert("Membro cadastrado!")
-    } catch (error: unknown) {
-      let errorMessage = 'Erro desconhecido'
-      if (axios.isAxiosError(error)) {
-        errorMessage = error.response?.data|| error.message
-      }
-      console.error("Erro ao cadastrar membro:", error)
-      alert("Erro ao cadastrar membro, " + errorMessage)
-    }
+    const tel = '5541999497870'
+    const mensagem = `Olá, meu nome é ${formData.nome} e gostaria de fazer parte da Igreja FIRE. Meu telefone é ${formData.telefone} e minha data de nascimento é ${formData.nascimento}.`
+    const url = `https://api.whatsapp.com/send?phone=${tel}&text=${encodeURIComponent(mensagem)}`
+    window.open(url, '_blank')
   }
 
   switch (type) {
     case 'inscricao':
       return (
-        <form onSubmit={handleSubmit} className={`${className} grid grid-cols-2 gap-4 my-12`}>
+        <form onSubmit={handleSubmitWhatsapp} className={`${className} grid grid-cols-2 gap-6 my-12`}>
           <Input 
             type="text" 
             className="col-span-2" 
             name="nome" 
-            placeholder="Nome" 
+            placeholder="Insira seu nome completo..."
             minLength={6} 
             onChange={handleChange}
+            value={formData.nome}
             required 
           />
           <Input 
             type="email" 
             className="col-span-2" 
             name="email" 
-            placeholder="Email" 
+            placeholder="Seu email..." 
             minLength={10} 
             onChange={handleChange}
-            required 
+            value={formData.email}
+            required
           />
           <Input 
             type="tel" 
+            className="col-span-2 md:col-span-1" 
             name="telefone" 
-            placeholder="Telefone" 
-            minLength={14}
+            placeholder="(41)..." 
+            pattern="^\(\d{2}\)\s\d{4,5}-\d{4}$"
             maxLength={15}
             onChange={reescreveTelefone}
-            value={telefone} 
+            value={telefone}
             required 
           />
           <Input 
-            type="date" 
-            name="nascimento"
-            onChange={handleChange}
+            type="text" 
+            className="col-span-2 md:col-span-1"
+            name="data de nascimento"
+            placeholder="00/00/0000"
+            pattern="^(0[1-9]|[12]\d|3[01])/(0[1-9]|1[0-2])/\d{4}$"
+            maxLength={10}
+            onChange={reescreveNascimento}
+            value={nascimento}
             required 
           />
-          {/* <textarea className="col-span-8 resize-none h-24 p-2" name="digite sua mensagem" placeholder="Digite sua mensagem aqui..."></textarea> */}
           <Input 
             type="submit" 
-            className="text-primary opacity-90 w-1/3 col-span-2 justify-self-center" 
-            value="Fazer inscrição"
+            className="text-primary opacity-90 col-span-2 justify-self-center" 
+            value="Entre em contato"
           />
         </form>
       )
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react"
+import { IoAlertCircle } from "react-icons/io5"
 
 interface InputProps {
     className?: string
     placeholder?: string
     pattern?: string
-    value?: string
+    data?: string
     minLength?: number
     maxLength?: number
     required?: boolean
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-    name?: 'nome' | 'email' | 'telefone' | 'nascimento' | 'mensagem'
+    value?: string
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void | undefined
+    name?: 'nome' | 'email' | 'telefone' | 'data de nascimento' | undefined
     type: 'text' | 'number' | 'submit' | 'tel' | 'email' | 'date'
 }
 
-export const Input = ({className = '', ...props}: InputProps) => {
+export const Input = ({ className = '', name, ...props }: InputProps) => {
   const tipoDeErro: (keyof ValidityState)[] = [
     'valueMissing',
     'typeMismatch',
@@ -40,26 +42,27 @@ export const Input = ({className = '', ...props}: InputProps) => {
     telefone: {
       valueMissing: "Telefone é obrigatório.",
       patternMismatch: "Por favor, insira um telefone válido.",
-      tooShort: "Por favor, insira um telefone válido.",
     },
-    nascimento: {
+    'data de nascimento': {
       valueMissing: "Data de nascimento é obrigatória.",
       patternMismatch: "Por favor, insira uma data de nascimento válida.",
     },
   }
 
   const [error, setError] = useState<string>("")
+  const [hiddenError, setHiddenError] = useState<boolean>(true)
 
   function verificaInput(e: React.FocusEvent<HTMLInputElement>) {
     let mensagem = ""
     e.target.setCustomValidity('')
 
-    const fieldName = e.target.name as keyof typeof mensagensDeErro
+    const fieldName = e.target.name as InputProps['name']
+    if (!fieldName) return
     let hasError = false
-    
+
     tipoDeErro.forEach(tipo => {
-      if (e.target.validity[tipo as keyof ValidityState]) {
-        mensagem = mensagensDeErro[fieldName][tipo as keyof typeof mensagensDeErro[typeof fieldName]]
+      if (e.target.validity[tipo] && tipo in mensagensDeErro[fieldName]) {
+        mensagem = mensagensDeErro[fieldName][tipo as keyof typeof mensagensDeErro[typeof fieldName]] || ""
         setError(`*${mensagem}`)
         hasError = true
       }
@@ -68,23 +71,65 @@ export const Input = ({className = '', ...props}: InputProps) => {
     if (!hasError) {
       setError("")
     }
-    
   }
-
+  
   return (
     <div className={`${className}`}>
-      <div className={`text-secondary ${props.type !== 'submit' && 'h-4'} relative`}>
-        <p className="absolute bottom-0">{error}</p>
+      <div className={`
+        relative flex place-items-center
+        ${props.type !== 'submit' && 'bg-primary'}
+        ${error && 'border-secondary border-2 border-opacity-50'}
+      `}
+      >
+        <input
+          id={name}
+          name={name}
+          className={`peer
+                      w-full placeholder-primary p-2 bg-transparent
+                      focus:placeholder-quaternary focus:placeholder-opacity-50 focus:outline-none focus:ring-0
+                      ${props.type === 'submit' ? 'cursor-pointer' : ''}
+                    `}
+          onBlur={verificaInput}
+          onInvalid={(e) => {
+            e.preventDefault()
+            setError('*Campo obrigatório.')
+          }}
+  
+          {...props}
+        />
+        {name && 
+          <label
+            htmlFor={name}
+            className={`
+              absolute left-2 transition-all duration-200 cursor-text
+              peer-placeholder-shown:top-2 peer-placeholder-shown:text-base
+              peer-focus:-top-5 peer-focus:text-xs peer-focus:opacity-100
+              ${props.value ? '-top-5 text-xs' : 'top-2'}
+              ${props.value && !error ? 'text-primary opacity-100' : 'text-quaternary opacity-50'}
+              ${error ? 'text-secondary peer-invalid:opacity-100' : 'peer-focus:text-primary'}
+            `}
+          >
+            {name}
+          </label>
+        }
+        {error &&
+          <div>
+            <IoAlertCircle 
+              className="w-6 h-6 text-secondary mr-2 cursor-pointer"
+              onClick={() => setHiddenError(!hiddenError)}
+            />
+          </div>
+        }
       </div>
-      <input 
-        className={`p-2 w-full ${props.type === 'submit' && 'cursor-pointer'}`}
-        {...props} 
-        onInvalid={ (e) => {
-          e.preventDefault()
-          setError('*Obrigatório.')
-        }} 
-        onBlur={ e => verificaInput(e) }
-      />
+      {error &&
+        <>
+          {!hiddenError &&
+            <div className="left-full w-full z-50 top-0 bg-secondary p-2">
+              <p className="text-sm text-primary">{error}</p>
+            </div>
+          }
+        </>
+      }
     </div>
   )
 }
